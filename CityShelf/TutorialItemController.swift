ba6a493c8ea3ba520fe84d6587e2462e5fe84632
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-04-30 21:00:36 -0400

    Resize skyline drawing

--- CityShelf/TutorialItemController.swift
+++ CityShelf/TutorialItemController.swift
@@ -37,81 +37,81 @@ class TutorialView: UIView {
             UIColor.blackColor().CGColor)
 
         // Horizontal line for the ground.
-        CGContextMoveToPoint(context, 0, 250)
-        CGContextAddLineToPoint(context, 400, 250)
+        CGContextMoveToPoint(context, 25, 250)
+        CGContextAddLineToPoint(context, 300, 250)
 
         // Rectangle for the first building.
         CGContextMoveToPoint(context, 50, 100)
-        CGContextAddLineToPoint(context, 150, 100)
-        CGContextAddLineToPoint(context, 150, 250)
+        CGContextAddLineToPoint(context, 125, 100)
+        CGContextAddLineToPoint(context, 125, 250)
         CGContextAddLineToPoint(context, 50, 250)
         CGContextAddLineToPoint(context, 50, 100)
         CGContextStrokePath(context)
 
         // Rectangle for the second building.
-        CGContextMoveToPoint(context, 150, 50)
-        CGContextAddLineToPoint(context, 250, 50)
-        CGContextAddLineToPoint(context, 250, 250)
-        CGContextAddLineToPoint(context, 150, 250)
-        CGContextAddLineToPoint(context, 150, 50)
+        CGContextMoveToPoint(context, 125, 50)
+        CGContextAddLineToPoint(context, 200, 50)
+        CGContextAddLineToPoint(context, 200, 250)
+        CGContextAddLineToPoint(context, 125, 250)
+        CGContextAddLineToPoint(context, 125, 50)
         CGContextStrokePath(context)
 
         // Rectangle for the third building.
-        CGContextMoveToPoint(context, 250, 150)
-        CGContextAddLineToPoint(context, 350, 150)
-        CGContextAddLineToPoint(context, 350, 250)
-        CGContextAddLineToPoint(context, 250, 250)
-        CGContextAddLineToPoint(context, 250, 50)
+        CGContextMoveToPoint(context, 200, 150)
+        CGContextAddLineToPoint(context, 275, 150)
+        CGContextAddLineToPoint(context, 275, 250)
+        CGContextAddLineToPoint(context, 200, 250)
+        CGContextAddLineToPoint(context, 200, 50)
         CGContextStrokePath(context)
 
         // Add in the six windows, leftmost to rightmost.
 
         // 1
         CGContextMoveToPoint(context, 50, 125)
-        CGContextAddLineToPoint(context, 100, 125)
-        CGContextAddLineToPoint(context, 100, 140)
+        CGContextAddLineToPoint(context, 80, 125)
+        CGContextAddLineToPoint(context, 80, 140)
         CGContextAddLineToPoint(context, 50, 140)
         CGContextAddLineToPoint(context, 50, 125)
         CGContextStrokePath(context)
 
         // 2
-        CGContextMoveToPoint(context, 100, 155)
-        CGContextAddLineToPoint(context, 150, 155)
-        CGContextAddLineToPoint(context, 150, 170)
-        CGContextAddLineToPoint(context, 100, 170)
-        CGContextAddLineToPoint(context, 100, 155)
+        CGContextMoveToPoint(context, 95, 155)
+        CGContextAddLineToPoint(context, 125, 155)
+        CGContextAddLineToPoint(context, 125, 170)
+        CGContextAddLineToPoint(context, 95, 170)
+        CGContextAddLineToPoint(context, 95, 155)
         CGContextStrokePath(context)
 
         // 3
-        CGContextMoveToPoint(context, 150, 110)
-        CGContextAddLineToPoint(context, 200, 110)
-        CGContextAddLineToPoint(context, 200, 125)
-        CGContextAddLineToPoint(context, 150, 125)
-        CGContextAddLineToPoint(context, 150, 110)
+        CGContextMoveToPoint(context, 125, 110)
+        CGContextAddLineToPoint(context, 155, 110)
+        CGContextAddLineToPoint(context, 155, 125)
+        CGContextAddLineToPoint(context, 125, 125)
+        CGContextAddLineToPoint(context, 125, 110)
         CGContextStrokePath(context)
 
         // 4
-        CGContextMoveToPoint(context, 200, 85)
-        CGContextAddLineToPoint(context, 250, 85)
-        CGContextAddLineToPoint(context, 250, 100)
-        CGContextAddLineToPoint(context, 200, 100)
+        CGContextMoveToPoint(context, 170, 85)
         CGContextAddLineToPoint(context, 200, 85)
+        CGContextAddLineToPoint(context, 200, 100)
+        CGContextAddLineToPoint(context, 170, 100)
+        CGContextAddLineToPoint(context, 170, 85)
         CGContextStrokePath(context)
 
         // 5
-        CGContextMoveToPoint(context, 200, 140)
-        CGContextAddLineToPoint(context, 250, 140)
-        CGContextAddLineToPoint(context, 250, 155)
-        CGContextAddLineToPoint(context, 200, 155)
+        CGContextMoveToPoint(context, 170, 140)
         CGContextAddLineToPoint(context, 200, 140)
+        CGContextAddLineToPoint(context, 200, 155)
+        CGContextAddLineToPoint(context, 170, 155)
+        CGContextAddLineToPoint(context, 170, 140)
         CGContextStrokePath(context)
 
         // 6
-        CGContextMoveToPoint(context, 250, 170)
-        CGContextAddLineToPoint(context, 300, 170)
-        CGContextAddLineToPoint(context, 300, 185)
-        CGContextAddLineToPoint(context, 250, 185)
-        CGContextAddLineToPoint(context, 250, 170)
+        CGContextMoveToPoint(context, 245, 170)
+        CGContextAddLineToPoint(context, 275, 170)
+        CGContextAddLineToPoint(context, 275, 185)
+        CGContextAddLineToPoint(context, 245, 185)
+        CGContextAddLineToPoint(context, 245, 170)
         CGContextStrokePath(context)
     }
 }
