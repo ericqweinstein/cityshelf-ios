2f4f06657682e44b2fbd78994d50a5de32a8a03e
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-05-28 18:59:28 -0400

    Remove back button text

--- CityShelf/NavigationViewController.swift
+++ CityShelf/NavigationViewController.swift
@@ -10,4 +10,10 @@ import UIKit
 
 /// Manages the NavigationViewController for searching.
 class NavigationViewController: UINavigationController {
+    // I don't really like this, but apparently setting a high
+    // negative offset is a common solution to removing the text
+    // on a navigation controller's back button. (EW 28 May 2015)
+    override func viewWillAppear(animated: Bool) {
+        UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffsetMake(0, -100), forBarMetrics: UIBarMetrics.Default)
+    }
 }
